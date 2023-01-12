1e3c2b75e0370527f6bc238e1b6a7af9b635e3d7
Polina Kulikova <pomidorishna@gmail.com>
2023-01-12 17:48:21 +0300

    Первые наработки

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// using static Library;
+
+// // Напишите программу, которая выводит массив из N элементов,
+// // заполненный нулями и единицами в случайном порядке.
+// // Самое левое число != 0
+// // Данный массив есть двоичное представление десятичного числа
+// // [1,0,1,1,0,1,0,0]
+// // Реализовать перевод двоичного числа в десятичное
+
+// // 1. Ввод N
+// // int <- (string)
+int GetNumberByUser(string text)
+{
+    Console.Write(text +": ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// // 2. Создание массива
+// // int[] <- (int)
+int[] CreateArray(int size);
+{
+    return new int[ size + 1] 
+}
+
+// // 3. Заполнение массива 0 и 1
+// // void <- (int[])
+
+// // 4. Печать массива
+// // string <- (int[])
+
+// // 5. Конвертация из 2-ого в 10-ое
+// // int <- (int[])
+
+// // 6. 10110100 >> 180
+// // string <- (int[] , int)
+// string GoodPrint(int[] bin, int dec)
+// {
+// return $"{String.Join("", bin)} >> {dec}";
+// }
+
+// Console.WriteLine(GoodPrint(new int[] { 1, 0, 1, 1, 1, 1, 0 }, 56));
--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ### Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// #### 452 -> 11
+// #### 82 -> 10
+// #### 9012 -> 12
+
+// Console.WriteLine("Введите число");
+// int UserNumber = Convert.ToInt32(Console.ReadLine());
+// string SumNumber = UserNumber.ToString();
+
+// int SumOfNumber()
+
+// Console.WriteLine(second[1]);
+
+// // int GetNumberByUser(string text)
+// // {
+// //     Console.Write(text +": ");
+// //     return Convert.ToInt32(Console.ReadLine());
+// // }
+
+Console.WriteLine("Введите число");
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int UserNumber){
+    
+    int digitsOfNumbers = Convert.ToString(UserNumber).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int count = 0; count < digitsOfNumbers; count++){
+      advance = UserNumber - UserNumber % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine(SumNumber);
