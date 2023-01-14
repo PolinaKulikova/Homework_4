aafbac010a88df20b7e38759891a35c35f49ae4e
Polina Kulikova <pomidorishna@gmail.com>
2023-01-15 02:07:33 +0300

    задача 1 готова

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -7,35 +7,69 @@
 // // [1,0,1,1,0,1,0,0]
 // // Реализовать перевод двоичного числа в десятичное
 
-// // 1. Ввод N
-// // int <- (string)
+ // Считать от пользователя данные
 int GetNumberByUser(string text)
 {
-    Console.Write(text +": ");
+    Console.WriteLine(text + ": ");
     return Convert.ToInt32(Console.ReadLine());
 }
 
-// // 2. Создание массива
-// // int[] <- (int)
-int[] CreateArray(int size);
+// Создание массива
+int[] CreateArray(int size)
 {
-    return new int[ size + 1]; 
+   return new int[size+1]; 
+}
+ 
+ 
+ void FillArray(int[] collection)      // Метод заполнения массива случайными числами от 0 до 1
+{
+    int length = collection.Length;
+    int index = 1;
+    collection[0] = 1;
+    while (index < length)
+    {
+        collection[index] = Random.Shared.Next(2); 
+        index++;
+    }
 }
 
-// // 3. Заполнение массива 0 и 1
-// // void <- (int[])
+string PrintArray(int[] collection)     // Метод печати массива
+{
+    string output = String.Empty;
+    int size = collection.Length;
+    for (int i = 0; i < collection.Length; i++)
+    {
+        output = output + $"{collection[i]} ";
+    }
+    return output;
+} 
 
-// // 4. Печать массива
-// // string <- (int[])
+int toDecimal(int[] collection)              //переводим в десятичную из двоичной
+{
+int count = collection.Length;
+int exponent = collection.Length - 1; 
+int result = 0;
+for (int i = 0; i < collection.Length; i++)
+{
+    if (collection[i] == 1)
+    {
+        result += Convert.ToInt32(Math.Pow(2, exponent));
+    }
+    exponent--;
+}
+return result;
+}
 
-// // 5. Конвертация из 2-ого в 10-ое
-// // int <- (int[])
+string GoodPrint(int[] collection, int dec)     //Красиво печатаем результат
+{
+    return $"{String.Join("", collection)} >> {dec}";
+}
 
-// // 6. 10110100 >> 180
-// // string <- (int[] , int)
-// string GoodPrint(int[] bin, int dec)
-// {
-// return $"{String.Join("", bin)} >> {dec}";
-// }
-
-// Console.WriteLine(GoodPrint(new int[] { 1, 0, 1, 1, 1, 1, 0 }, 56));
+int countByUser = GetNumberByUser("Введите N");
+int[] array = CreateArray(countByUser);
+FillArray(array);                                   // Вызов метода заполнения  массива
+PrintArray(array);                                  // Вызов метода печати массива
+int result = toDecimal(array);                      //кладем результат конвертации в int
+string decNumber = PrintArray(array);               // делаем строку из результата распечатки
+Console.WriteLine(decNumber);            
+Console.WriteLine(GoodPrint(array, result));
--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,38 +1,32 @@
-﻿// ### Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// #### 452 -> 11
-// #### 82 -> 10
-// #### 9012 -> 12
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
 
-// Console.WriteLine("Введите число");
-// int UserNumber = Convert.ToInt32(Console.ReadLine());
-// string SumNumber = UserNumber.ToString();
+ // Считать от пользователя данные
+int GetNumberByUser(string text)
+{
+    Console.WriteLine(text + ": ");
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-// int SumOfNumber()
+// Создание массива
+int[] CreateArray(int size)
+{
+   return new int[size+1]; 
+}
 
-// Console.WriteLine(second[1]);
 
-// // int GetNumberByUser(string text)
-// // {
-// //     Console.Write(text +": ");
-// //     return Convert.ToInt32(Console.ReadLine());
-// // }
 
-Console.WriteLine("Введите число");
-int UserNumber = Convert.ToInt32(Console.ReadLine());
 
-  int SumNumber(int UserNumber){
-    
-    int digitsOfNumbers = Convert.ToString(UserNumber).Length;
-    int advance = 0;
-    int result = 0;
 
-    for (int count = 0; count < digitsOfNumbers; count++){
-      advance = UserNumber - UserNumber % 10;
-      result = result + (UserNumber - advance);
-      UserNumber = UserNumber / 10;
-    }
-   return result;
-  }
 
-int sumNumber = SumNumber(UserNumber);
-Console.WriteLine(UserNumber);
+
+
+
+
+
+
+
+ int countByUser = GetNumberByUser("Введите число");
+int[] array = CreateArray(countByUser);
