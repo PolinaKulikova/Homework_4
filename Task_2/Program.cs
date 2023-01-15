08182b8d41836346e126a3a9afa4f8b3f74be0e3
Polina Kulikova <pomidorishna@gmail.com>
2023-01-15 03:21:19 +0300

    Задача 2 готова

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -3,30 +3,27 @@
 // 82 -> 10
 // 9012 -> 12
 
- // Считать от пользователя данные
-int GetNumberByUser(string text)
+int GetNumberByUser(string text)                      // Получили число от пользователя
 {
     Console.WriteLine(text + ": ");
     return Convert.ToInt32(Console.ReadLine());
 }
 
-// Создание массива
-int[] CreateArray(int size)
+int SumOfNumbers(int countByUser)                     // Разбили по разрядам , переведя в строку
 {
-   return new int[size+1]; 
-}
+    int count = Convert.ToString(countByUser).Length;
+    int shift = 0;
+    int result = 0;
 
+    for (int i = 0; i < count; i++)
+    {
+      shift = countByUser - countByUser % 10;
+      result = result + (countByUser - shift);
+      countByUser = countByUser / 10;
+    }
+   return result;
+  }
 
-
-
-
-
-
-
-
-
-
-
-
- int countByUser = GetNumberByUser("Введите число");
-int[] array = CreateArray(countByUser);
+int countByUser = GetNumberByUser("Введите число, если хочешь узнать сумму его цифр");
+int sum = SumOfNumbers(countByUser);
+Console.WriteLine(sum);
